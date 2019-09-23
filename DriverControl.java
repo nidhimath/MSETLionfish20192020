394d6080024f7f438968162e8fdaff1136a8459d
nidhimath <51516936+nidhimath@users.noreply.github.com>
2019-09-22 19:05:17 -0700

    Update DriverControl.java

--- DriverControl.java
+++ DriverControl.java
@@ -1,20 +1,22 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
-public class DriverControl extends LinearOpMode {
+@TeleOp(name="DriverControl_FirstTryTwoMotors", group="lions")
+public class DriverControl_FirstTryTwoMotors extends LinearOpMode {
     //initiating motors
     private DcMotor frontLeft;
     private DcMotor frontRight;
-    private DcMotor backLeft;
-    private DcMotor backRight;
+    //private DcMotor backLeft;
+    //private DcMotor backRight;
     //private DcMotor m1,m2,m3,m4; //these motors will be used for other things
     public void runOpMode(){
-        frontLeft  = hardwareMap.get(DcMotor.class, "frontLeft");
-        frontRight  = hardwareMap.get(DcMotor.class, "frontRight");
-        backLeft  = hardwareMap.get(DcMotor.class, "backLeft");
-        backRight  = hardwareMap.get(DcMotor.class, "backRight");
+        frontLeft  = hardwareMap.get(DcMotor.class, "fl");
+        frontRight  = hardwareMap.get(DcMotor.class, "fr");
+        //backLeft  = hardwareMap.get(DcMotor.class, "backLeft");
+        //backRight  = hardwareMap.get(DcMotor.class, "backRight");
 
         telemetry.addData("Status", "Initialized");
         telemetry.update();
@@ -24,11 +26,11 @@ public class DriverControl extends LinearOpMode {
         double lrRightStick=0;
         double fbRightStick=0;
         frontRight.setDirection(DcMotor.Direction.REVERSE);
-        backRight.setDirection(DcMotor.Direction.REVERSE);
+        //backRight.setDirection(DcMotor.Direction.REVERSE);
         //put all movement code in here
         while(opModeIsActive()){
-            fbLeftStick = -this.gamepad1.left_stick_y;
-            fbRightStick = -this.gamepad1.right_stick_y;
+            fbLeftStick = this.gamepad1.left_stick_y;
+            fbRightStick = this.gamepad1.right_stick_y;
             lrLeftStick = -this.gamepad1.left_stick_x; //if turning the wrong direction reverse the sign
             lrRightStick = -this.gamepad1.right_stick_x; //if turning the wrong direction reverse the sign
             /*
@@ -36,59 +38,59 @@ public class DriverControl extends LinearOpMode {
             strafe(lrLeftStick);
             turn(lrRightStick);
             */
-            //tank(fbLeftStick, fbRightStick);
+            tank(fbLeftStick, fbRightStick);
             telemetry.addData("Status", "Running");
             telemetry.update();
         }
     }
     public void tank(double fbLeftStick, double fbRightStick){
         frontLeft.setPower(fbLeftStick);
-        backLeft.setPower(fbLeftStick);
-        backRight.setPower(fbRightStick);
-        frontRight.setPower(fbRightstick);
+        //backLeft.setPower(fbLeftStick);
+        //backRight.setPower(fbRightStick);
+        frontRight.setPower(fbRightStick);
         telemetry.addData("Target Power", fbLeftStick);
         telemetry.addData("Target Power", fbRightStick);
         telemetry.addData("Motor Power", frontLeft.getPower());
         telemetry.addData("Motor Power", frontRight.getPower());
-        telemetry.addData("Motor Power", backLeft.getPower());//hi test
-        telemetry.addData("Motor Power", backRight.getPower());
+        //telemetry.addData("Motor Power", backLeft.getPower());//hi test
+        //telemetry.addData("Motor Power", backRight.getPower());
         
     }
     public void movingFB(double fbLeftStick){
         
         frontLeft.setPower(fbLeftStick);
         frontRight.setPower(fbLeftStick);
-        backLeft.setPower(fbLeftStick);
-        backLeft.setPower(fbLeftStick);
+        //backLeft.setPower(fbLeftStick);
+        //backLeft.setPower(fbLeftStick);
         telemetry.addData("Target Power", fbLeftStick);
         telemetry.addData("Motor Power", frontLeft.getPower());
         telemetry.addData("Motor Power", frontRight.getPower());
-        telemetry.addData("Motor Power", backLeft.getPower());
-        telemetry.addData("Motor Power", backRight.getPower());
+        //telemetry.addData("Motor Power", backLeft.getPower());
+        //telemetry.addData("Motor Power", backRight.getPower());
     }
     public void strafe(double lrLeftStick){
         
         frontLeft.setPower(lrLeftStick);
-        backLeft.setPower(-lrLeftStick);
+        //backLeft.setPower(-lrLeftStick);
         frontRight.setPower(-lrLeftStick);
-        backRight.setPower(lrLeftStick);
+        //backRight.setPower(lrLeftStick);
         telemetry.addData("Target Power", lrLeftStick);
         telemetry.addData("Motor Power", frontLeft.getPower());
         telemetry.addData("Motor Power", frontRight.getPower());
-        telemetry.addData("Motor Power", backLeft.getPower());//hi test
-        telemetry.addData("Motor Power", backRight.getPower());
+        //telemetry.addData("Motor Power", backLeft.getPower());//hi test
+        //telemetry.addData("Motor Power", backRight.getPower());
     }
     public void turn(double lrRightStick){
         
         frontLeft.setPower(lrRightStick);
-        backLeft.setPower(lrRightStick);
+        //backLeft.setPower(lrRightStick);
         frontRight.setPower(-lrRightStick);
-        backRight.setPower(-lrRightStick);
+        //backRight.setPower(-lrRightStick);
         telemetry.addData("Target Power", lrRightStick);
         telemetry.addData("Motor Power", frontLeft.getPower());
         telemetry.addData("Motor Power", frontRight.getPower());
-        telemetry.addData("Motor Power", backLeft.getPower());
-        telemetry.addData("Motor Power", backRight.getPower());
+        //telemetry.addData("Motor Power", backLeft.getPower());
+        //telemetry.addData("Motor Power", backRight.getPower());
     }
     /*
     public void m1(double m1){
